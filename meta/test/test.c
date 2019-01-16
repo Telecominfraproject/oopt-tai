70f91b4a34db55042d257f70c35c77feef96e26f
Wataru Ishida <ishida@nel-america.com>
2019-01-16 14:09:06 -0800

    meta/test: refactor unit testing
    Signed-off-by: Wataru Ishida <ishida@nel-america.com>

--- meta/test/test.c
+++ meta/test/test.c
@@ -1,8 +1,37 @@
 #include "tai.h"
 #include "taimetadata.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <string.h>
 
-int test(char *buffer) {
+int testSerializeModuleOperStatus() {
+    int ret;
+    char buf[128] = {0};
+    ret = tai_serialize_module_oper_status(buf, TAI_MODULE_OPER_STATUS_READY, NULL);
+    if ( ret < 0 ) {
+        return ret;
+    }
+    ret = strcmp(buf, "TAI_MODULE_OPER_STATUS_READY");
+    if ( ret != 0 ) {
+        return -1;
+    }
+    return 0;
+}
+
+int testDeserializeModuleOperStatus() {
+    int ret, value;
+    ret = tai_deserialize_module_oper_status("TAI_MODULE_OPER_STATUS_INITIALIZE", &value, NULL);
+    if ( ret < 0 ) {
+        return ret;
+    }
+    if ( TAI_MODULE_OPER_STATUS_INITIALIZE != value ) {
+        return -1;
+    }
+    return 0;
+}
+
+int testSerializeAttributeEnum() {
+    int ret;
+    char buf[128] = {0};
     tai_attribute_t attr = {0};
     tai_serialize_option_t option = {
         .valueonly = true,
@@ -10,10 +39,20 @@ int test(char *buffer) {
     const tai_attr_metadata_t* meta = tai_metadata_get_attr_metadata(TAI_OBJECT_TYPE_MODULE, TAI_MODULE_ATTR_OPER_STATUS);
     attr.id = TAI_MODULE_ATTR_OPER_STATUS;
     attr.value.s32 = TAI_MODULE_OPER_STATUS_READY;
-    return tai_serialize_attribute(buffer, meta, &attr, &option);
+    ret = tai_serialize_attribute(buf, meta, &attr, &option);
+    if ( ret <  0 ) {
+        return -1;
+    }
+    ret = strcmp(buf, "TAI_MODULE_OPER_STATUS_READY");
+    if ( ret != 0 ) {
+        return -1;
+    }
+    return 0;
 }
 
-int test2(char *buffer) {
+int testSerializeAttributeFloat() {
+    int ret;
+    char buf[128] = {0};
     tai_attribute_t attr = {0};
     tai_serialize_option_t option = {
         .valueonly = true,
@@ -21,10 +60,20 @@ int test2(char *buffer) {
     const tai_attr_metadata_t* meta = tai_metadata_get_attr_metadata(TAI_OBJECT_TYPE_MODULE, TAI_MODULE_ATTR_TEMP);
     attr.id = TAI_MODULE_ATTR_TEMP;
     attr.value.flt = 1.10;
-    return tai_serialize_attribute(buffer, meta, &attr, &option);
+    ret = tai_serialize_attribute(buf, meta, &attr, &option);
+    if ( ret < 0 ) {
+        return -1;
+    }
+    ret = strcmp(buf, "1.100000");
+    if ( ret != 0 ) {
+        return -1;
+    }
+    return 0;
 }
 
-int test3(char *buffer) {
+int testSerializeAttributeEnumList() {
+    int ret;
+    char buf[128] = {0};
     tai_attribute_t attr = {0};
     tai_serialize_option_t option = {
         .valueonly = false,
@@ -38,56 +87,72 @@ int test3(char *buffer) {
     attr.id = TAI_NETWORK_INTERFACE_ATTR_TX_ALIGN_STATUS;
     attr.value.s32list.count = 2;
     attr.value.s32list.list = (int32_t*)&list;
-
-    return tai_serialize_attribute(buffer, meta, &attr, &option);
-}
-
-int test4() {
-    const tai_attr_metadata_t* meta;
-    meta = tai_metadata_get_attr_metadata_by_attr_id_name("TAI_NETWORK_INTERFACE_ATTR_TX_LASER_FREQ");
-    printf("%p\n", meta);
+    ret = tai_serialize_attribute(buf, meta, &attr, &option);
+    if ( ret < 0 ) {
+        return -1;
+    }
+    ret = strcmp(buf, "tx-align-status | loss|out");
+    if ( ret != 0 ) {
+        return -1;
+    }
     return 0;
 }
 
-int test5() {
+int testGetAttrMetadataByAttrIdName() {
+    const tai_attr_metadata_t* meta;
+    meta = tai_metadata_get_attr_metadata_by_attr_id_name("TAI_NETWORK_INTERFACE_ATTR_TX_LASER_FREQ");
+    if ( meta == NULL ) {
+        return -1;
+    }
+    if ( meta->attrid != TAI_NETWORK_INTERFACE_ATTR_TX_LASER_FREQ ) {
+        return -1;
+    }
+ 
+    return 0;
+}
+
+int testDeserializeNetworkInterfaceAttr() {
     int32_t value;
     int ret;
     tai_serialize_option_t option = {
         .human = true,
     };
     ret = tai_deserialize_network_interface_attr("tx-laser-freq", &value, &option);
-    printf("%d, %d\n", value, ret);
-    return ret;
+    if ( ret < 0 ) {
+        return -1;
+    }
+    if ( value != TAI_NETWORK_INTERFACE_ATTR_TX_LASER_FREQ ) {
+        return -1;
+    }
+    return 0;
 }
 
+typedef int (*testF)();
+
+testF tests[] = {
+    testSerializeModuleOperStatus,
+    testDeserializeModuleOperStatus,
+    testSerializeAttributeEnum,
+    testSerializeAttributeFloat,
+    testSerializeAttributeEnumList,
+    testGetAttrMetadataByAttrIdName,
+    testDeserializeNetworkInterfaceAttr,
+    NULL,
+};
+
 int main() {
-    int ret, value;
-    char buf[128] = {0};
+    int i = 0, ret;
 
-    ret = tai_serialize_module_oper_status(buf, TAI_MODULE_OPER_STATUS_READY, NULL);
+    tai_metadata_log_level = TAI_LOG_LEVEL_DEBUG;
 
-    printf("%s, %d\n", buf, ret);
-    ret = tai_deserialize_module_oper_status("TAI_MODULE_OPER_STATUS_INITIALIZE", &value, NULL);
-    printf("%d, %d\n", value, ret);
-
-    ret = test(buf);
-    printf("%s, %d\n", buf, ret);
-
-    ret = test2(buf);
-    printf("%s, %d\n", buf, ret);
-
-    ret = test3(buf);
-    printf("%s, %d\n", buf, ret);
-
-    ret = test4();
-    if ( ret < 0 ) {
-        return ret;
+    while (true) {
+        if( tests[i] == NULL ) {
+            break;
+        }
+        ret = tests[i++]();
+        if ( ret < 0 ) {
+            return ret;
+        }
     }
-
-    ret = test5();
-    if ( ret < 0 ) {
-        return ret;
-    }
-
     return 0;
 }
