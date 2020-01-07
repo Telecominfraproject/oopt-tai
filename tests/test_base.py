4b0822cdfa7cb8d4297fb0d374869a64ebc875d9
Wataru Ishida <ishida@nel-america.com>
2020-01-07 12:18:56 +0000

    tests: support changing module location to test
    Signed-off-by: Wataru Ishida <ishida@nel-america.com>

--- tests/test_base.py
+++ tests/test_base.py
@@ -5,6 +5,10 @@ import os
 import time
 import taish
 
+TAI_TEST_MODULE_LOCATION = os.environ.get('TAI_TEST_MODULE_LOCATION', '0')
+if TAI_TEST_MODULE_LOCATION == '':
+    TAI_TEST_MODULE_LOCATION = '0'
+
 def output_reader(proc):
     for line in iter(proc.stdout.readline, b''):
         print('taish-server: {}'.format(line.decode('utf-8')), end='')
@@ -29,19 +33,19 @@ class TestTAI(unittest.TestCase):
         cli = taish.Client()
         m = cli.list()
         self.assertNotEqual(m, None)
-        self.assertTrue('0' in m)
-        module = m['0']
+        self.assertTrue(TAI_TEST_MODULE_LOCATION in m)
+        module = m[TAI_TEST_MODULE_LOCATION]
         print('module oid: 0x{:x}'.format(module.oid))
 
     def test_get_module(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         self.assertNotEqual(m, None)
         print('module oid: 0x{:x}'.format(m.oid))
 
     def test_get_netif(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         self.assertNotEqual(m, None)
         netif = m.get_netif()
         self.assertNotEqual(netif, None)
@@ -49,7 +53,7 @@ class TestTAI(unittest.TestCase):
 
     def test_get_hostif(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         self.assertNotEqual(m, None)
         hostif = m.get_hostif()
         self.assertNotEqual(hostif, None)
@@ -57,7 +61,7 @@ class TestTAI(unittest.TestCase):
 
     def test_module(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         for metadata in m.list_attribute_metadata():
             try:
                 v = m.get(metadata)
@@ -67,7 +71,7 @@ class TestTAI(unittest.TestCase):
 
     def test_netif(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         netif = m.get_netif()
         for metadata in netif.list_attribute_metadata():
             try:
@@ -78,7 +82,7 @@ class TestTAI(unittest.TestCase):
 
     def test_netif_set_output_power(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         netif = m.get_netif()
         netif.set('output-power', '-4')
         self.assertEqual(round(float(netif.get('output-power'))), -4)
@@ -87,7 +91,7 @@ class TestTAI(unittest.TestCase):
 
     def test_netif_set_modulation_format(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         netif = m.get_netif()
         netif.set('modulation-format', 'dp-64-qam')
         self.assertEqual(netif.get('modulation-format'), 'dp-64-qam')
@@ -96,7 +100,7 @@ class TestTAI(unittest.TestCase):
 
     def test_hostif(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         hostif = m.get_hostif()
         for metadata in hostif.list_attribute_metadata():
             try:
@@ -107,7 +111,7 @@ class TestTAI(unittest.TestCase):
 
     def test_hostif_set_fec(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         hostif = m.get_hostif()
         hostif.set('fec-type', 'rs')
         self.assertEqual(hostif.get('fec-type'), 'rs')
@@ -118,7 +122,7 @@ class TestTAI(unittest.TestCase):
 
     def test_hostif_set_loopback(self):
         cli = taish.Client()
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         hostif = m.get_hostif()
         hostif.set('loopback-type', 'shallow')
         self.assertEqual(hostif.get('loopback-type'), 'shallow')
@@ -131,67 +135,67 @@ class TestTAI(unittest.TestCase):
         cli = taish.Client()
         l = cli.list()
         self.assertNotEqual(l, None)
-        self.assertTrue('0' in l)
-        module = l['0']
+        self.assertTrue(TAI_TEST_MODULE_LOCATION in l)
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertNotEqual(module.oid, 0)
         self.assertEqual(len(module.netifs), 1)
         self.assertEqual(len(module.hostifs), 2)
 
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         netif = m.get_netif()
         cli.remove(netif.oid)
         l = cli.list()
-        module = l['0']
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertEqual(len(module.netifs), 0)
 
         hostif = m.get_hostif()
         cli.remove(hostif.oid)
         l = cli.list()
-        module = l['0']
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertEqual(len(module.hostifs), 1)
 
         hostif = m.get_hostif(1)
         cli.remove(hostif.oid)
         l = cli.list()
-        module = l['0']
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertEqual(len(module.hostifs), 0)
 
         cli.remove(module.oid)
         l = cli.list()
-        module = l['0']
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertEqual(module.oid, 0)
 
     def test_create(self):
         self.test_remove()
         cli = taish.Client()
         with self.assertRaises(Exception):
-            cli.get_module('0')
-        cli.create('module', [('location', 0)])
+            cli.get_module(TAI_TEST_MODULE_LOCATION)
+        cli.create('module', [('location', TAI_TEST_MODULE_LOCATION)])
         l = cli.list()
         self.assertNotEqual(l, None)
-        self.assertTrue('0' in l)
-        module = l['0']
+        self.assertTrue(TAI_TEST_MODULE_LOCATION in l)
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertNotEqual(module.oid, 0)
         self.assertEqual(len(module.netifs), 0)
         self.assertEqual(len(module.hostifs), 0)
 
-        m = cli.get_module('0')
+        m = cli.get_module(TAI_TEST_MODULE_LOCATION)
         self.assertEqual(int(m.get('num-network-interfaces')), 1)
         self.assertEqual(int(m.get('num-host-interfaces')), 2)
 
         cli.create('netif', [('index', 0)], m.oid)
         l = cli.list()
-        module = l['0']
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertEqual(len(module.netifs), 1)
 
         cli.create('hostif', [('index', 0)], m.oid)
         l = cli.list()
-        module = l['0']
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertEqual(len(module.hostifs), 1)
 
         cli.create('hostif', [('index', 1)], m.oid)
         l = cli.list()
-        module = l['0']
+        module = l[TAI_TEST_MODULE_LOCATION]
         self.assertEqual(len(module.hostifs), 2)
 
 
