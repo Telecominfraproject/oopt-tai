7997bfc23b5ec61fb6402a20822998170e01a9d7
Wataru Ishida <wataru.ishid@gmail.com>
2021-09-13 06:59:06 +0000

    meta: support adding custom enum values to TAI attributes
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

---
+++ tools/framework/examples/basic/custom/netif.h
@@ -0,0 +1,14 @@
+#ifndef __TAI_BASIC_NETIF__
+#define __TAI_BASIC_NETIF__
+
+#include <tai.h>
+
+/**
+ * @custom tai_network_interface_fec_type_t
+ */
+typedef enum _tai_custom_fec_type_t
+{
+    TAI_NETWORK_INTERFACE_FEC_TYPE_PROPRIETARY = 10,
+} tai_custom_fec_type_t;
+
+#endif
