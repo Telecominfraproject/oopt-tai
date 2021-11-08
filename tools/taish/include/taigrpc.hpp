Telecominfraproject/oopt-tai

commit 3448aca2a1e71825005b9127dc815bdeea367d8d

taish: serialize TAI API calls
Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

tools/taish/include/taigrpc.hpp
@@ -123,7 +123,7 @@ class TAIServiceImpl final : public taish::TAI::Service {
         }
         const tai_api_method_table_t* const m_api;
         std::map<std::pair<tai_object_id_t, tai_attr_id_t>, std::shared_ptr<TAINotifier>> m_notifiers;
-        std::mutex m_mtx; // mutex for m_notifiers
+        std::mutex m_mtx; // mutex for serialized TAI API calls
 };
 
 const tai_attr_metadata_t* const get_metadata(tai_meta_api_t* meta_api, const tai_metadata_key_t * const key, tai_attr_id_t attr_id);

tools/taish/lib/server.cpp
@@ -316,6 +316,8 @@ static tai_serialize_option_t convert_serialize_option(const taish::SerializeOpt
             }
         }
 
+        std::unique_lock<std::mutex> lk(m_mtx);
+
         switch (type) {
         case TAI_OBJECT_TYPE_MODULE:
             return m_api->module_api->get_module_attribute(oid, attr);
@@ -355,6 +357,7 @@ static tai_serialize_option_t convert_serialize_option(const taish::SerializeOpt
     auto ret = TAI_STATUS_SUCCESS;
     try {
         auto attr = std::make_unique<tai::Attribute>(meta, v, &option);
+        std::unique_lock<std::mutex> lk(m_mtx);
         switch (type) {
         case TAI_OBJECT_TYPE_MODULE:
             ret = m_api->module_api->set_module_attribute(oid, attr->raw());
@@ -380,6 +383,7 @@ static tai_serialize_option_t convert_serialize_option(const taish::SerializeOpt
     auto id = request->attr_id();
     auto type = tai_object_type_query(oid);
     tai_status_t ret;
+    std::unique_lock<std::mutex> lk(m_mtx);
 
     switch (type) {
     case TAI_OBJECT_TYPE_HOSTIF:
@@ -614,6 +618,7 @@ void monitor_callback(void* context, tai_object_id_t oid, uint32_t attr_count, t
     auto meta = get_metadata(m_api->meta_api, &key, TAI_MODULE_ATTR_LOCATION);
     tai::S_Attribute loc;
     if ( type != TAI_OBJECT_TYPE_MODULE ) {
+        std::unique_lock<std::mutex> lk(m_mtx);
         auto getter = [&](tai_attribute_t* attr) -> tai_status_t {
             return m_api->module_api->get_module_attribute(mid, attr);
         };
@@ -659,7 +664,13 @@ void monitor_callback(void* context, tai_object_id_t oid, uint32_t attr_count, t
         list.emplace_back(*(v->raw()));
     }
 
-    auto ret = create(&oid, list.size(), list.data());
+    tai_status_t ret;
+
+    {
+        std::unique_lock<std::mutex> lk(m_mtx);
+        ret = create(&oid, list.size(), list.data());
+    }
+
     if ( ret == TAI_STATUS_SUCCESS ) {
         response->set_oid(oid);
         m_api->object_update(type, oid, true);
@@ -672,6 +683,7 @@ void monitor_callback(void* context, tai_object_id_t oid, uint32_t attr_count, t
     auto oid = request->oid();
     auto type = tai_object_type_query(oid);
     tai_status_t ret;
+    std::unique_lock<std::mutex> lk(m_mtx);
 
     switch (type) {
     case TAI_OBJECT_TYPE_MODULE:
@@ -686,16 +698,14 @@ void monitor_callback(void* context, tai_object_id_t oid, uint32_t attr_count, t
     default:
         ret = TAI_STATUS_NOT_SUPPORTED;
     }
+
     if ( ret == TAI_STATUS_SUCCESS ) {
-        {
-            std::unique_lock<std::mutex> lk(m_mtx);
-            auto it = m_notifiers.begin();
-            while  ( it != m_notifiers.end() ) {
-                if ( it->first.first == oid ) {
-                    it = m_notifiers.erase(it);
-                } else {
-                    it++;
-                }
+        auto it = m_notifiers.begin();
+        while  ( it != m_notifiers.end() ) {
+            if ( it->first.first == oid ) {
+                it = m_notifiers.erase(it);
+            } else {
+                it++;
             }
         }
         m_api->object_update(type, oid, false);

commit 8fb7c73b72880444f51feb7ea4d918e478afb53f

taish: use a separate mutex for API serialization
a bug introduced by 3448aca2a1e71825005b9127dc815bdeea367d8d

m_mtx for API serialization
m_notifiers_mtx to protect m_notifiers

Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

tools/taish/include/taigrpc.hpp
@@ -122,8 +122,11 @@ class TAIServiceImpl final : public taish::TAI::Service {
             return m_notifiers[key];
         }
         const tai_api_method_table_t* const m_api;
-        std::map<std::pair<tai_object_id_t, tai_attr_id_t>, std::shared_ptr<TAINotifier>> m_notifiers;
         std::mutex m_mtx; // mutex for serialized TAI API calls
+
+        std::map<std::pair<tai_object_id_t, tai_attr_id_t>, std::shared_ptr<TAINotifier>> m_notifiers;
+        std::mutex m_notifiers_mtx; // mutex to protect m_notifiers
+
 };
 
 const tai_attr_metadata_t* const get_metadata(tai_meta_api_t* meta_api, const tai_metadata_key_t * const key, tai_attr_id_t attr_id);

tools/taish/lib/server.cpp
@@ -517,7 +517,7 @@ void monitor_callback(void* context, tai_object_id_t oid, uint32_t attr_count, t
             }
 
             {
-                std::unique_lock<std::mutex> lk(m_mtx);
+                std::unique_lock<std::mutex> lk(m_notifiers_mtx);
                 if ( m_notifiers.find(key) == m_notifiers.end() ) {
                     return Status(StatusCode::UNKNOWN, "object is removed");
                 }
@@ -545,9 +545,10 @@ void monitor_callback(void* context, tai_object_id_t oid, uint32_t attr_count, t
     }
 
     {
-        std::unique_lock<std::mutex> lk(m_mtx);
+        std::unique_lock<std::mutex> lk(m_notifiers_mtx);
 
         if ( notifier->size() == 1 ) {
+            std::unique_lock<std::mutex> lk(m_mtx);
             attr.value.notification.notify = nullptr;
             attr.value.notification.context = nullptr;
             switch (type) {
@@ -683,27 +684,32 @@ void monitor_callback(void* context, tai_object_id_t oid, uint32_t attr_count, t
     auto oid = request->oid();
     auto type = tai_object_type_query(oid);
     tai_status_t ret;
-    std::unique_lock<std::mutex> lk(m_mtx);
-
-    switch (type) {
-    case TAI_OBJECT_TYPE_MODULE:
-        ret = m_api->module_api->remove_module(oid);
-        break;
-    case TAI_OBJECT_TYPE_NETWORKIF:
-        ret = m_api->netif_api->remove_network_interface(oid);
-        break;
-    case TAI_OBJECT_TYPE_HOSTIF:
-        ret = m_api->hostif_api->remove_host_interface(oid);
-        break;
-    default:
-        ret = TAI_STATUS_NOT_SUPPORTED;
+    {
+        std::unique_lock<std::mutex> lk(m_mtx);
+        switch (type) {
+        case TAI_OBJECT_TYPE_MODULE:
+            ret = m_api->module_api->remove_module(oid);
+            break;
+        case TAI_OBJECT_TYPE_NETWORKIF:
+            ret = m_api->netif_api->remove_network_interface(oid);
+            break;
+        case TAI_OBJECT_TYPE_HOSTIF:
+            ret = m_api->hostif_api->remove_host_interface(oid);
+            break;
+        default:
+            ret = TAI_STATUS_NOT_SUPPORTED;
+        }
     }
 
     if ( ret == TAI_STATUS_SUCCESS ) {
+        std::unique_lock<std::mutex> lk(m_notifiers_mtx);
         auto it = m_notifiers.begin();
         while  ( it != m_notifiers.end() ) {
             if ( it->first.first == oid ) {
+                auto notifier = it->second;
+                tai_notification_t empty;
                 it = m_notifiers.erase(it);
+                notifier->notify(empty); // signal the deletion
             } else {
                 it++;
             }